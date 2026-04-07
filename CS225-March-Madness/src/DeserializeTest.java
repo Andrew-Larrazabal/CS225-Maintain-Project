295f5cc4762571a0b379c8613a3bca39cf19c4eb
Joshua Staub <18756667+Carbonyte@users.noreply.github.com>
2026-04-07 14:03:32 -0400

    Reindent files for consistent 4 space width

--- CS225-March-Madness/src/DeserializeTest.java
+++ CS225-March-Madness/src/DeserializeTest.java
@@ -4,20 +4,20 @@ import java.io.ObjectInputStream;
 /*Hillary Ssemakula: This class tests deserialization of a file named hillary.ser */
 public class DeserializeTest
 {
-  public static void main(String[] args)
-  {
-    Bracket hillary = null;
-    FileInputStream inStream = null;
-    ObjectInputStream in = null;
-    try 
+    public static void main(String[] args)
     {
-      inStream = new FileInputStream("hillary.ser");
-      in = new ObjectInputStream(inStream);
-      hillary = (Bracket) in.readObject();
-      in.close();
-    } catch (Exception e) {
-      e.printStackTrace();
+        Bracket hillary = null;
+        FileInputStream inStream = null;
+        ObjectInputStream in = null;
+        try
+        {
+            inStream = new FileInputStream("hillary.ser");
+            in = new ObjectInputStream(inStream);
+            hillary = (Bracket) in.readObject();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for(String team: hillary.getBracket()) { System.out.println(team); };
     }
-    for(String team: hillary.getBracket()) { System.out.println(team); };
-  }
 }
